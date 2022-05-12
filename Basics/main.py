b1ab368250a5be01b1629917a576944819b90384
philo-Essien <71226700+philo-Essien@users.noreply.github.com>
2022-05-11 21:39:03 -0400

    new

---
+++ Basics/main.py
@@ -0,0 +1,43 @@
+import builtins
+from pprint import pprint
+
+#pprint(dir(builtins))
+
+# print("hello world")
+# print(len("hello world"))
+# print(sum([3,2,6,7,1]))
+# print(input("please enter user name"))
+
+#Variable
+first_name = "philo"
+last_name = "Essien"
+age = 90
+# print(age)
+# print(type(age))
+# print(last_name)
+# print(type(last_name))
+# print(type(first_name))
+
+## the expected output is
+## my first name is philo and my last name is Essien and i am 90 years old
+
+#print(f"my first name is {first_name} and my last name is {last_name} and i am {age} years old")
+school = "kojitechs"
+class_room = "python"
+#print(f"my name is philo, and i school in {school}, and i learn {class_room}")
+
+
+## How to name your variable== avoid using python builtins to name your variable
+## variables are case sensitive
+## no space in between your variable === use underscore
+
+#Data type (found in builtin functions)
+# Builtin Data types
+## str == text, figures all in qoutes("")
+## int == numbers = (123)
+## list [] == ["apple", "banana", "apple"]
+## float == (12.3)
+## boolean  bool == True or False
+## dictionary dic{} == map = {"name": "philo", "age": 90}
+## set {} == set_numbers = {1, 2, 3, 4, 5} {"apple", "banan", "cherry"}
+## None
